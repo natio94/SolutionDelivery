240bef92961c0bac5651cb9cd673c9120ecc448e
NohamBoulanouar <noham.boulanouar-----delaby@efrei.net>
2026-06-21 21:47:33 +0200

    add algo ParcoursProfondeur

--- src/main/java/backend/algo/ParcoursProfondeur.java
+++ src/main/java/backend/algo/ParcoursProfondeur.java
@@ -3,44 +3,35 @@ package backend.algo;
 import backend.models.Arete;
 import backend.models.Quai;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class ParcoursProfondeur {
 
-    public List<Quai> trouverChemin(Quai depart, Quai arrivee) {
-        Set<Quai> visites = new HashSet<>();
-        List<Quai> chemin = new ArrayList<>();
 
-        if (parcoursEnProfondeur(depart, arrivee, visites, chemin)) {
-            return  chemin;
-        }else{
-            return new ArrayList<>();
-        }
-    }
-
-    private boolean parcoursEnProfondeur(Quai courant, Quai arrivee, Set<Quai> visites, List<Quai> chemin) {
-        visites.add(courant);
-        chemin.add(courant);
-
-        if (courant.equals(arrivee)) {
+    public boolean estConnexe(List<Quai> tousLesQuais) {
+        if (tousLesQuais == null || tousLesQuais.isEmpty() || tousLesQuais.size() == 1) {
             return true;
         }
+
+        Set<Quai> visites = new HashSet<>();
+
+        Quai depart = tousLesQuais.getFirst();
+        dfs(depart, visites);
+
+        return visites.size() == new HashSet<>(tousLesQuais).size();
+    }
+
+    private void dfs(Quai courant, Set<Quai> visites) {
+        visites.add(courant);
 
         for (Arete arete : courant.getVoisins()) {
             Quai voisin = arete.getDestination();
 
             if (!visites.contains(voisin)) {
-                if (parcoursEnProfondeur(voisin, arrivee, visites, chemin)) {
-                    return true;
-                }
+                dfs(voisin, visites);
             }
         }
-
-
-        chemin.removeLast();
-        return false;
     }
 }
