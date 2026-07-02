a5afa23a4c32fb2761c51e15d540502046427535
Fanto974 <144560009+Fanto974@users.noreply.github.com>
2026-07-02 15:12:08 +0200

    Delete src/main/java/backend directory

--- src/main/java/backend/gtfs/StopGTFS.java
+++
@@ -1,6 +0,0 @@
-package backend.gtfs;
-
-public record StopGTFS(String stopId, String name, double lat, double lon, int zoneId,
-                       int locationType, String parentStation, boolean wheelchair) {
-	public String getId(){ return this.stopId; }
-}
--- src/main/java/backend/gtfs/StopTimeGTFS.java
+++
@@ -1,5 +0,0 @@
-package backend.gtfs;
-
-public record StopTimeGTFS(String tripId, String stopId, String time, int stop_sequence) {
-	public String getId(){ return this.tripId; }
-}
